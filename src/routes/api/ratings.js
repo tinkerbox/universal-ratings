08361769d559ac7c22b5c5d887db41e487b8ac5a
Jaryl Sim <jaryl.sim@me.com>
2020-09-07 00:42:32 +0800

    Test several locations for server endpoints

--- src/routes/api/ratings.js
+++ src/routes/api/ratings.js
@@ -1,3 +1,3 @@
 export default (req, res) => {
-  res.json({ name: 'John', email: 'john@example.com' });
+  res.json({ name: 'John (api)', email: 'john@example.com' });
 };
---
+++ src/routes/ratings.js
@@ -0,0 +1,3 @@
+export default (req, res) => {
+  res.json({ name: 'John (root)', email: 'john@example.com' });
+};
